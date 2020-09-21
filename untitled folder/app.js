f9ea0fc52bfe513e48f9f63ca833ecf4eebb8211
Sarah Gambles <sarahgambles14@gmail.com>
2020-09-21 17:27:56 -0600

    generated html file

--- untitled folder/app.js
+++ untitled folder/app.js
@@ -1,15 +1,31 @@
+const fs = require('fs');
+const generatePage = require('./portfolio-generator/src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(proilfeDataArr[i]);
-    }
-    console.lot('===========')
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
 
-printProfileData(profileDataArgs);
+  fs.writeFile('index.html', generatePage(name, github), err => {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  })
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the 'profileDataArr' parameter?
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(proilfeDataArr[i]);
+//     }
+//     console.lot('===========')
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+// printProfileData(profileDataArgs);
