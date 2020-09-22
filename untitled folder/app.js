833d2c12d8c8a131ab5a364b5c953acbe86ec091
Sarah Gambles <sarahgambles14@gmail.com>
2020-09-22 13:39:52 -0600

    installed npm

--- untitled folder/app.js
+++ untitled folder/app.js
@@ -1,15 +1,36 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(proilfeDataArr[i]);
+const inquirer = require('inquirer');
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
     }
-    console.lot('===========')
+])
+.then(answers => console.log(answers));
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
+// const fs = require('fs');
+// const generatePage = require('./src');
 
-printProfileData(profileDataArgs);
+// const pageHTML = generatePage(portfolioData);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+    // if (err) throw new Error(err);
+
+  //  console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the 'profileDataArr' parameter?
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(proilfeDataArr[i]);
+//     }
+//     console.lot('===========')
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+// printProfileData(profileDataArgs);
