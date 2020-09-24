3a3db951e2913117d66f115440db46c52a18c8ca
Sarah Gambles <sarahgambles14@gmail.com>
2020-09-23 21:15:09 -0600

    made app more functional

--- untitled folder/app.js
+++ untitled folder/app.js
@@ -1,36 +1,150 @@
 const inquirer = require('inquirer');
-inquirer.prompt([
+const { FindValueSubscriber } = require('rxjs/internal/operators/find');
+
+
+const promptUser = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input', 
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-])
-.then(answers => console.log(answers));
+]);
+};
+
+const promptProject = portfolioData => {
+
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    console.log(`
+    
+    =================
+    Add a New Project
+    =================
+`)
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'link', 
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub link to your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'confirm',
+            name: 'feature', 
+            message: 'Would you like to feature this project?',
+            default: FindValueSubscriber,
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]);
+};
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
+
+
 
 
 // const fs = require('fs');
-// const generatePage = require('./src');
+// const generatePage = require('./portfolio-generator/src/page-template');
 
-// const pageHTML = generatePage(portfolioData);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-    // if (err) throw new Error(err);
 
-  //  console.log('Portfolio complete! Check out index.html to see the output!');
+
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// // Notice the lack of parentheses around the 'profileDataArr' parameter?
-// const printProfileData = (profileDataArr) => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(proilfeDataArr[i]);
-//     }
-//     console.lot('===========')
-
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-
-// };
-
-// printProfileData(profileDataArgs);
